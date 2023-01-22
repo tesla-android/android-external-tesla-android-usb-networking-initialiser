5def8670d5021462cc383f23db2e46d3a6a59e12
Michał Gapiński <mike@gapinski.eu>
2023-01-22 18:04:11 +0100

    Support for Alcatel IK40V

--- tesla-android-usb-networking-initialiser.c
+++ tesla-android-usb-networking-initialiser.c
@@ -12,6 +12,9 @@ char* appleInitialisationCommand = "usbmuxd -v -f -X";
 char* alcatelIK41XXId = "1bbb:00b6"
 char* alcatelIK41XXInitialisationCommand = 'echo -e "AT+USBMODE=1\r\n" > /dev/ttyUSB2';
 
+char* alcatelIK40VId = "1bbb:f000"
+char* alcatelIK40VInitialisationCommand = "/vendor/bin/usb_modeswitch -c /vendor/tesla-android/usb_modeswitch/1bbb-f000.conf";
+
 int connectedDevicesSize = 0;
 char **connectedDeviceIds;
 
@@ -42,6 +45,9 @@ void initialiseDevice(char * deviceId) {
 	} else if(strstr(deviceId, alcatelIK41XXId)) {
 		printf("Converting Alcatel IK41 from mbim to rndis");
 		system(alcatelIK41XXInitialisationCommand);
+	} else if(strstr(deviceId, alcatelIK40VId)) {
+		printf("Initialising Alcatel IK40V");
+		system(alcatelIK40VInitialisationCommand);
 	}
 }
 
